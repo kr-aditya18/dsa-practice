b0eb113cc5cac61a883a4ac67e64ce563061ae63
kr-aditya18 <vermaadiitya13678@gmail.com>
2026-02-24 00:18:15 +0530

    Solved check array if sorted  with brute,optimal

--- Day-1/removeDuplicated.cpp
+++ Day-1/removeDuplicated.cpp
@@ -13,11 +13,23 @@ void removeDupBrute(int arr[],int n){
         index++;
     }
 }
+
+int removeDupOptimal(int arr[],int n){
+    int i = 0;
+    for(int j = 1; j<=n-1; j++){
+        if(arr[j]!=arr[i]){
+            arr[i+1] = arr[j];
+            i++;
+        }
+    }
+    return i+1; 
+}
 int main(){
     int n;
     cin>>n;
     int arr[n];
     for(int i = 0;i<n;i++) cin>>arr[i];
     removeDupBrute(arr,n);
+    removeDupOptimal(arr,n);
     return 0;
 }
